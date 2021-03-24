98d8c8d9c44f0609a52d5d19fb2d59ea4d514484
wickedvic <viksipart@gmail.com>
2021-03-23 21:23:50 -0400

    icon, description, location and dropdown added

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location'
+import WeatherInfo from './components/WeatherInfo'
+import UnitsPicker from './components/UnitsPicker'
+import {colors} from './utils/index'
 
 const WEATHER_API_KEY = '73b5efe60f23ab4fb889e73a97e68e90'
 const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
@@ -15,8 +18,11 @@ export default function App() {
 
   useEffect(() => {
     load()
-  }, [])
+  }, [unitsSystem])
+
   async function load(){
+    setCurrentWeather(null)
+    setErrorMessage(null)
     try {
       let {status} = await Location.requestPermissionsAsync()
       if(status != "granted") {
@@ -46,19 +52,28 @@ export default function App() {
     }
   }
 if (currentWeather) {
-  const { 
-    main : { temp },
-  } = currentWeather
+ 
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <UnitsPicker setUnitsSystems={setUnitsSystems} unitsSystem={unitsSystem}/>
+          <WeatherInfo currentWeather={currentWeather}/>
+        </View>
     </View>
     )
-  }else{
+  }else if (errorMessage){
       return (
         <View style={styles.container}>
           <Text>{errorMessage}</Text>
+          <StatusBar style="auto" />
+        </View>
+      )
+    }
+    else{
+      return(
+        <View style={styles.container}>
+          <ActivityIndicator size="large" color={colors.PRIMARY_COLOR}/>
           <StatusBar style="auto" />
         </View>
       )
@@ -70,8 +85,10 @@ if (currentWeather) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  main: {
+    justifyContent: 'center',
+    flex: 1
+  }
 });
